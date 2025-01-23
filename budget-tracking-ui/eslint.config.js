d3789fadbb425525d346c6b49f9b9c0e4d9ebe82
Le Tran Anh Vu <vule5726@gmail.com>
2025-01-23 22:38:51 +0200

    ui: config eslint done

---
+++ budget-tracking-ui/eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+    css: true,
+    html: true,
+  },
+)
--- budget-tracking-ui/src/App.vue
+++ budget-tracking-ui/src/App.vue
@@ -5,24 +5,32 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo">
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <button class="bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700 ml-4 cursor-pointer">Click me</button>
-  <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-  <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://tailwindcss.com/_next/static/media/erin-lindford.90b9d461.jpg" alt="" />
-  <div class="space-y-2 text-center sm:text-left">
-    <div class="space-y-0.5">
-      <p class="text-lg font-semibold text-black">Erin Lindford</p>
-      <p class="font-medium text-gray-500">Product Engineer</p>
+  <button class="bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700 ml-4 cursor-pointer">
+    Click me
+  </button>
+  <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4">
+    <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://tailwindcss.com/_next/static/media/erin-lindford.90b9d461.jpg" alt="">
+    <div class="space-y-2 text-center sm:text-left">
+      <div class="space-y-0.5">
+        <p class="text-lg font-semibold text-black">
+          Erin Lindford
+        </p>
+        <p class="font-medium text-gray-500">
+          Product Engineer
+        </p>
+      </div>
+      <button class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700">
+        Message
+      </button>
     </div>
-    <button class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700">Message</button>
   </div>
-</div>
 </template>
 
 <style scoped>
@@ -33,7 +41,7 @@ import HelloWorld from './components/HelloWorld.vue'
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
