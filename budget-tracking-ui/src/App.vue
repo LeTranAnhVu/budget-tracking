592349fa42a7e0ba31551f3ba75f6c8b2ef2fdd9
Le Tran Anh Vu <vule5726@gmail.com>
2025-01-23 12:36:36 +0200

    ui: setup tailwind

--- budget-tracking-ui/src/App.vue
+++ budget-tracking-ui/src/App.vue
@@ -12,6 +12,17 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <button class="bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700 ml-4 cursor-pointer">Click me</button>
+  <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+  <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://tailwindcss.com/_next/static/media/erin-lindford.90b9d461.jpg" alt="" />
+  <div class="space-y-2 text-center sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg font-semibold text-black">Erin Lindford</p>
+      <p class="font-medium text-gray-500">Product Engineer</p>
+    </div>
+    <button class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700">Message</button>
+  </div>
+</div>
 </template>
 
 <style scoped>
